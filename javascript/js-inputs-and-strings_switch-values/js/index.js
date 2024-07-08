5cd4d2a0dbe0c4ccfeb6ab45d37b7425ccdbfa7a
Maximilian Hammes <maximview@gmail.com>
2024-07-08 16:02:26 +0200

    switch challenge update

--- javascript/js-inputs-and-strings_switch-values/js/index.js
+++ javascript/js-inputs-and-strings_switch-values/js/index.js
@@ -15,20 +15,22 @@ Hint: the HTML entity for the switch button is: &updownarrow
 
 const firstInput = document.querySelector('[data-js="first-input"]');
 const secondInput = document.querySelector('[data-js="second-input"]');
-const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
-const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const upperCaseButton = document.querySelector('[data-js="button-uppercase"]');
+const lowerCaseButton = document.querySelector('[data-js="button-lowercase"]');
 const switchButton = document.querySelector('[data-js="button-switch"]');
 
-uppercaseButton.addEventListener("click", () => {
+upperCaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
   secondInput.value = secondInput.value.toUpperCase();
 });
 
-lowercaseButton.addEventListener("click", () => {
+lowerCaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
   secondInput.value = secondInput.value.toLowerCase();
 });
 
 switchButton.addEventListener("click", () => {
-  //////////////
+  const variable = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = variable;
 });
