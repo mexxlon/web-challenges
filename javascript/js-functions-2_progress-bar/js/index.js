62509a950c18552600bb8a764bdf4010345aa8b1
Maximilian Hammes <maximview@gmail.com>
2024-07-08 21:34:08 +0200

    add prograss bar challenge

--- javascript/js-functions-2_progress-bar/js/index.js
+++ javascript/js-functions-2_progress-bar/js/index.js
@@ -19,6 +19,20 @@ For that:
  (HINT: you can change the width of an element like this: element.style.width = '10px')
 */
 
-const progressBar = document.querySelector('[data-js="progress-bar"]');
+const calculateScrollPercentage = () => {
+  const scroll = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.body.clientHeight;
 
-function calculateScrollPercentage() {}
+  const scrollPercentage = (scroll / (documentHeight - windowHeight)) * 100;
+  return scrollPercentage;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const progressBar = document.querySelector('[data-js="progress-bar"]');
+
+  document.addEventListener("scroll", () => {
+    const scrollPercentage = calculateScrollPercentage();
+    progressBar.style.width = `${scrollPercentage}%`;
+  });
+});
