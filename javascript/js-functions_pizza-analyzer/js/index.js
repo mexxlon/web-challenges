05cd5a7f9e3ca0c72e752a4f2f699b12013419ef
Maximilian Hammes <maximview@gmail.com>
2024-07-09 11:19:40 +0200

    add pizza analyzer js objects and arrays challenge

--- javascript/js-functions_pizza-analyzer/js/index.js
+++ javascript/js-functions_pizza-analyzer/js/index.js
@@ -5,19 +5,19 @@ const pizza1 = document.querySelector('[data-js="pizza-1"]');
 const pizzaInput2 = document.querySelector('[data-js="pizza-input-2"]');
 const pizza2 = document.querySelector('[data-js="pizza-2"]');
 const outputSection = document.querySelector('[data-js="output-section"]');
-
 const output = document.querySelector('[data-js="output"]');
 
-pizzaInput1.addEventListener("input", () => {
-  // write your code here
-});
+pizzaInput1.addEventListener("input", () => {});
 
 pizzaInput2.addEventListener("input", () => {
   // write your code here
 });
 
-// Task 1
-// define the function calculatePizzaGain here
+// CALCULATE PIZZA GAIN
+
+function calculatePizzaGain(diameter1, diamteter2) {
+  const pi = math.PI;
+}
 
 // Task 2
 // define the function updatePizzaDisplay here
