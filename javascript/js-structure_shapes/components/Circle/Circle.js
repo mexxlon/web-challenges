fb6bc993cdd2bf1aba9e27a51bf6e0a9d82c8952
Maximilian Hammes <maximview@gmail.com>
2024-07-12 21:10:15 +0200

    add first version of the structure shape challenge

---
+++ javascript/js-structure_shapes/components/Circle/Circle.js
@@ -0,0 +1,1 @@
+import { getRandomColor } from "../../utils/randomColor";
---
+++ javascript/js-structure_shapes/utils/randomColor.js
@@ -0,0 +1,5 @@
+export { getRandomColor };
+
+function getRandomColor() {
+  return `hsl(${Math.floor(Math.random() * 360)} 80% 55%)`;
+}
