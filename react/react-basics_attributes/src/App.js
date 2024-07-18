400875467cb204fe1a64984ceb202b5a1c9d652e
Maximilian Hammes <maximview@gmail.com>
2024-07-18 11:58:14 +0200

    update

--- react/react-basics_attributes/src/App.js
+++ react/react-basics_attributes/src/App.js
@@ -8,9 +8,10 @@ export default function App() {
 function NewArticle() {
   return (
     <article className="article">
-      <h2 className="article__title">
-        This is the test text. I like avocados!
-      </h2>
+      <h2 className="article__title">Its Avocado time!</h2>
+      <br />
+      We all like Avocados!
+      <br />
       <label htmlFor="inputField">Input:</label>
       <input id="inputField" />
       <a className="article__link" href="https://de.wikipedia.org/wiki/Avocado">
